7200c9d1e8dcbe195c921b25fd8e9076ef0de4cf
Mihail_Dolgushev <dolguschev.misha@ya.ru>
2022-01-23 00:16:52 +0300

    add world generation

---
+++ Assets/Scripts/GridManagement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridManagement : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Tile[] tiles;
+    [SerializeField] private int mapWidth;
+    [SerializeField] private int mapHeight;
+
+    private void Start()
+    {
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+                tilemap.SetTile(new Vector3Int(i, j, 0), tiles[Mathf.RoundToInt(Random.Range(0, 2))]);
+            }
+        }
+        for (int i = 0; i < mapWidth; i++)
+        {
+            tilemap.SetTile(new Vector3Int(i, -1, 0), tiles[Mathf.RoundToInt(Random.Range(4, 6))]);
+        }
+        Camera.main.transform.position = new Vector3(mapWidth / 2 + .5f, mapHeight / 2, -10);
+    }
+
+}
